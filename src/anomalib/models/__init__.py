53eb9541cdef1a3f183d44d2406192b69e693af2
tntchack <tnt.chack@gmail.com>
2023-11-12 10:58:58 +0330

    saving progress

--- src/anomalib/models/__init__.py
+++ src/anomalib/models/__init__.py
@@ -12,7 +12,7 @@ from importlib import import_module
 from omegaconf import DictConfig, ListConfig
 from torch import load
 
-from anomalib.models.ai_vad import AiVad
+# from anomalib.models.ai_vad import AiVad
 from anomalib.models.cfa import Cfa
 from anomalib.models.cflow import Cflow
 from anomalib.models.components import AnomalyModule
@@ -43,7 +43,7 @@ __all__ = [
     "ReverseDistillation",
     "Rkde",
     "Stfpm",
-    "AiVad",
+    # "AiVad",
     "EfficientAD",
 ]
 
