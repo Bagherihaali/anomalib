e798db438140822bf8c40866cfbd0213ac9d86b1
tntchack <tnt.chack@gmail.com>
2023-12-03 16:44:45 +0330

    fix bug in mlflow model logging

--- src/anomalib/utils/callbacks/mlflow.py
+++ src/anomalib/utils/callbacks/mlflow.py
@@ -3,11 +3,13 @@ from pytorch_lightning.callbacks import ModelCheckpoint, Callback
 
 
 class MLFlowModelCheckpoint(Callback):
-    def __init__(self, dirpath, requirements_path):
+    def __init__(self, dirpath, requirements_path, mlflow_tracking_uri):
         super().__init__()
         self.dirpath = dirpath
-        self.best_model_path = None
-        self.last_saved_model = None
+        self.tracking_uri = mlflow_tracking_uri
+        self.path_to_best_model = ''
+        self.last_saved_model = ''
+        self.best_score = None
         self.requirements_path = requirements_path
 
     def on_validation_end(self, trainer, pl_module):
@@ -15,11 +17,15 @@ class MLFlowModelCheckpoint(Callback):
             if isinstance(callback, ModelCheckpoint):
                 # there is two model checkpoint callbacks
                 if callback.best_model_path:
-                    self.best_model_path = callback.best_model_path
+                    callb = callback
+                    self.path_to_best_model = callback.best_model_path
+                    self.best_score = callback.best_model_score
 
-        if self.best_model_path and self.last_saved_model != self.best_model_path:
-            model = pl_module.load_from_checkpoint(self.best_model_path)
+        if self.path_to_best_model != '' and self.last_saved_model != self.path_to_best_model:
+            model = pl_module.load_from_checkpoint(self.path_to_best_model)
             conda_env = self.requirements_path + r'\environment.yaml'
-            # pip_requirements = self.requirements_path + r'\requirements.txt'
-            mlflow.pytorch.log_model(model, str(self.dirpath), conda_env=conda_env)
-            self.last_saved_model = self.best_model_path
+            mlflow.set_tracking_uri(self.tracking_uri)
+            with mlflow.start_run(run_id=pl_module.logger._run_id):
+                mlflow.pytorch.log_model(model, str(self.dirpath), conda_env=conda_env)
+            pl_module.logger.log_metrics({'best_score': float(self.best_score)})
+            self.last_saved_model = self.path_to_best_model
