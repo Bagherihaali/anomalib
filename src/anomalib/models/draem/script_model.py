05263d94bedce707367eac1fdeebc3a7ef6b7a11
tntchack <tnt.chack@gmail.com>
2024-07-03 10:19:32 +0330

    rename sspcab parameter so it will be compatible with run parameters

--- src/anomalib/models/draem/script_model.py
+++ src/anomalib/models/draem/script_model.py
@@ -89,9 +89,9 @@ class SSPCAB(nn.Module):
 class DraemModelScript(nn.Module):
     """DRAEM PyTorch model consisting of the reconstructive and discriminative sub networks."""
 
-    def __init__(self, sspcab: bool = False) -> None:
+    def __init__(self, enable_sspcab: bool = False) -> None:
         super().__init__()
-        self.reconstructive_subnetwork = ReconstructiveSubNetwork(in_channels=1, out_channels=1, sspcab=sspcab)
+        self.reconstructive_subnetwork = ReconstructiveSubNetwork(in_channels=1, out_channels=1, sspcab=enable_sspcab)
         self.discriminative_subnetwork = DiscriminativeSubNetwork(in_channels=2, out_channels=2)
 
     def forward(self, batch: Tensor):
